3dae372b2bc185fc0b1bac378f6f1783706e9ce1
VictordalaM <delamotte.v@gamil.com>
2023-12-11 12:48:44 +0100

    erste aufgabe fertig

---
+++ logical_operator/assets/js/main.js
@@ -0,0 +1,30 @@
+let x = 10
+let y = 15
+let z = 20
+
+if(x > z && x > y){
+    console.log(true)
+} else {console.log(false)}
+
+if(x != y){
+    console.log(true)
+} else {console.log(false)}
+
+if(z < y || z > x){
+    console.log(true)
+} else {console.log(false)}
+
+if(x == z || x != y){
+    console.log(true)
+} else {console.log(false)}
+
+if(x >= 10 && y <= 10){
+    console.log(true)
+} else {console.log(false)}
+
+if(x*z < 100 || x * y > 100){
+    console.log(true)
+} else {console.log(false)}
+
+
+
